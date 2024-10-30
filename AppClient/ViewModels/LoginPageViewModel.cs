500c60403d74bc379ea71ee61c89c2f187c51ada
User <User@LAB2-14>
2024-10-30 09:31:27 +0200

    Added to ViewModel.

--- AppClient/ViewModels/LoginPageViewModel.cs
+++ AppClient/ViewModels/LoginPageViewModel.cs
@@ -1,8 +1,46 @@
+using System.Windows.Input;
+
 namespace AppClient.ViewModels;
 
 public class LoginPageViewModel : ViewModelBase
-{ 
-	public LoginPageViewModel()
+{
+    //public AppClientWebApi service;
+    private readonly IServiceProvider serviceProvider;
+
+    public ICommand LoginCommand { get; set; }
+    public ICommand GoToSignUpCommand { get; set; }
+    private string mail;
+    public string Mail
+    {
+        get => mail;
+        set
+        {
+            if (mail != value)
+            {
+                mail = value;
+                OnPropertyChanged(nameof(Mail));
+                // can add more logic here like email validation etc.
+                // can add error message property and set it here
+            }
+        }
+    }
+    private string password;
+    public string Password
+    {
+        get => password;
+        set
+        {
+            if (password != value)
+            {
+                password = value;
+                OnPropertyChanged(nameof(Password));
+                // can add more logic here like email validation etc.
+                // can add error message property and set it here
+            }
+        }
+    }
+
+    public LoginPageViewModel()
 	{
 		
 	}
---
+++ AppClient/Views/SignUp.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppClient.Views;
+
+public partial class SignUp : ContentPage
+{
+	public SignUp()
+	{
+		InitializeComponent();
+	}
+}
